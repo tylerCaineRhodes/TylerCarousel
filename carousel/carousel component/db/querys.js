7f2ccde04ac7af001fd276bb0267b093a0822264
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-01-27 14:27:43 -0600

    Connect to Wowes database

--- carousel/carousel component/db/querys.js
+++ carousel/carousel component/db/querys.js
@@ -1,3 +1,18 @@
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'Wowes'
+});
+
+connection.connect(err => {
+  if(err){
+    console.log('something wrong with connecting to the database')
+  } else {
+    console.log('now connected to your database')
+  }
+})
 
 
