141d100a5d37d53c3cdbd57acfb0783944703716
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-01-30 12:45:55 -0600

    Add font awesome and make buttons dynamically disappear: gs

--- carousel/carousel component/client/src/index.js
+++ carousel/carousel component/client/src/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './App.jsx'
+import App from './App.jsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
