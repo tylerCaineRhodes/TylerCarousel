553013c7816a29f19e4d655db92adbd8ffaead8f
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-01-30 13:58:12 -0600

    Made change

--- carousel/carousel component/client/src/components/Item.jsx
+++ carousel/carousel component/client/src/components/Item.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 function Item ({name, image, category, rating}){
+  //refactor into class component
+  //import star rating library
+  //set the state to rating
   return (
     <div className='singleItem'>
       <img src={image}></img>
       <span id="itemName"><b>{name}</b></span>
-      <span id="rating">rating:{rating}</span>
+      <span id="rating">rating--> {rating}</span>
       <span id="smallSave">SAVE 46% thru 01/31/2020</span>
     </div>
   )
