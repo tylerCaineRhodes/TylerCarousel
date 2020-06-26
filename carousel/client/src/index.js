814d88060903bbf1562bab39ed324ccd3085fd2e
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-06-26 12:49:38 -0500

    Clean code spacings in index file

--- carousel/client/src/index.js
+++ carousel/client/src/index.js
@@ -3,12 +3,33 @@ import React from 'react';
 import App from './App.jsx';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faCheckSquare, faCoffee, faBacon, faAngleLeft, faAngleRight, faDragon, faDiceD20, faGavel,faGrimace, faHamsa, faHatWizard} from '@fortawesome/free-solid-svg-icons';
- 
-library.add(fab, faCheckSquare, faCoffee, faBacon, faAngleLeft, faAngleRight, faDragon, faDiceD20, faGavel, faGrimace, faHamsa, faHatWizard)
+import {
+  faCheckSquare,
+  faCoffee,
+  faBacon,
+  faAngleLeft,
+  faAngleRight,
+  faDragon,
+  faDiceD20,
+  faGavel,
+  faGrimace,
+  faHamsa,
+  faHatWizard,
+} from '@fortawesome/free-solid-svg-icons';
 
-
- 
+library.add(
+  fab,
+  faCheckSquare,
+  faCoffee,
+  faBacon,
+  faAngleLeft,
+  faAngleRight,
+  faDragon,
+  faDiceD20,
+  faGavel,
+  faGrimace,
+  faHamsa,
+  faHatWizard
+);
 
 ReactDOM.render(<App />, document.getElementById('carousel'));
-
