3b23899ccd5d007f6fb58f91c2b817322597216b
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-06-26 12:34:09 -0500

    Get rid of console logs in server routes

--- carousel/server/server.js
+++ carousel/server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const port = 3005;
 const app = express();
-var cors = require('cors')
+const cors = require('cors')
 const db = require('../db/querys.js');
 
 app.use(cors());
@@ -14,31 +14,26 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.get('/wowStuff', (req, res) => {
   db.getAllData((err, data) => {
     if(err){
-      console.log('problem getting all tasks in the server')
       res.send(err)
     } else {
       res.send(data)
     }
   })
 })
-app.get('/wowStuff/category', (req, res) => {
-  console.log('this is params --->', req.query)
 
+app.get('/wowStuff/category', (req, res) => {
   db.getCategories(req.query.id, (err, data) => {
     if(err){
-      console.log('problem getting categories in the server')
       res.send(err)
     } else {
       res.send(data)
     }
   })
 })
-app.get('/wowStuff/item', (req, res) => {
-  console.log('this is params --->', req.query)
 
+app.get('/wowStuff/item', (req, res) => {
   db.getItem(req.query.id, (err, data) => {
     if(err){
-      console.log('problem getting categories in the server')
       res.send(err)
     } else {
       res.send(data)
@@ -47,13 +42,5 @@ app.get('/wowStuff/item', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`yo, dude, listen-  you're connected to ${port}`);
+  console.log(`you're connected to ${port}`);
 })
-
-
-
-
-
-
-
-
