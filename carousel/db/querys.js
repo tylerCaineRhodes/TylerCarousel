57fb44649058230970a681574664164554369314
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-07-11 14:32:25 -0500

    Connect to old db instance

--- carousel/db/querys.js
+++ carousel/db/querys.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  // port: process.env.PORT,
+  port: process.env.PORT,
   database: process.env.DB_NAME,
 });
 
