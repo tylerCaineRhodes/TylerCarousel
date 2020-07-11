957ad5874c43be2a1f948d33003e127f2bd9de61
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-07-11 18:15:03 -0500

    Connect to lorem

--- carousel/db/querys.js
+++ carousel/db/querys.js
@@ -6,6 +6,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.PORT,
   multipleStatements: true,
 });
 
